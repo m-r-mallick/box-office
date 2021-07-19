2a70f4fe98120d52e724b87e0b9681e95dc921d9
codeworm <mrmblogger@gmail.com>
2021-07-20 01:23:37 +0530

    added search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-   return <MainPageLayout>This is home page</MainPageLayout>;
+   const [input, setInput] = useState('');
+   const onInputChange = event => {
+      setInput(event.target.value);
+   };
+   const onPressEnter = event => {
+      if (event.keyCode === 13) {
+         onSearch();
+      }
+   };
+   const onSearch = async () => {
+      const rawData = await fetch(
+         `https://api.tvmaze.com/search/shows?q=${input}`
+      );
+      const jsonData = await rawData.json();
+      console.log(jsonData);
+   };
+   return (
+      <MainPageLayout>
+         <input
+            type="text"
+            onChange={event => onInputChange(event)}
+            value={input}
+            onKeyDown={event => onPressEnter(event)}
+         />
+         <button onClick={onSearch}>Search</button>
+      </MainPageLayout>
+   );
 };
 
 export default Home;
