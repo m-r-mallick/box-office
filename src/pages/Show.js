6fe27c9685cbfee7edaf2bfcdd03570658fe8829
codeworm <mrmblogger@gmail.com>
2021-07-20 06:34:36 +0530

    loading and error display added

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,17 +5,34 @@ import { apiGet } from '../misc/config';
 const Show = () => {
    const { id } = useParams();
    const [show, setShow] = useState(null);
+   const [isLoading, setLoading] = useState(true);
+   const [error, setError] = useState(false);
 
    useEffect(() => {
-      apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(res =>
-         setShow(res)
-      );
+      let isMounted = true;
+      apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+         .then(res => {
+            if (isMounted) {
+               setShow(res);
+               setLoading(false);
+            }
+         })
+         .catch(() => {
+            setError(true);
+            setLoading(false);
+         });
+
+      return () => (isMounted = false);
    }, [id]);
 
    return (
       <div>
-         This is the show page for this ID - {id}
-         <pre>{JSON.stringify(show, undefined, 3)}</pre>{' '}
+         <h1>show page</h1>
+         {isLoading && <h2>Loading...</h2>}
+         {error}
+         {!error && !isLoading && (
+            <pre>{JSON.stringify(show, undefined, 3)}</pre>
+         )}
       </div>
    );
 };
