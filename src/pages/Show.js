35e913c150b2bfeb7e1b7d0f1afd0c51636f849d
codeworm <mrmblogger@gmail.com>
2021-07-20 23:06:36 +0530

    added read more components with formatting

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,38 +1,95 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
+const initialState = {
+   show: null,
+   isLoading: true,
+   error: false,
+};
+const reducer = (prevState, action) => {
+   switch (action.type) {
+      case 'SET_SHOW':
+         return {
+            ...prevState,
+            show: action.payload,
+         };
+      case 'SET_LOADING':
+         return {
+            ...prevState,
+            isLoading: action.payload,
+         };
+      case 'SET_ERROR':
+         return {
+            ...prevState,
+            error: action.payload,
+         };
+
+      default:
+         return prevState;
+   }
+};
 const Show = () => {
    const { id } = useParams();
-   const [show, setShow] = useState(null);
-   const [isLoading, setLoading] = useState(true);
-   const [error, setError] = useState(false);
+
+   const [{ show, isLoading, error }, dispatch] = useReducer(
+      reducer,
+      initialState
+   );
 
    useEffect(() => {
       let isMounted = true;
       apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
          .then(res => {
             if (isMounted) {
-               setShow(res);
-               setLoading(false);
+               dispatch({ type: 'SET_SHOW', payload: res });
+               dispatch({ type: 'SET_LOADING', payload: false });
             }
          })
          .catch(() => {
-            setError(true);
-            setLoading(false);
+            dispatch({ type: 'SET_ERROR', payload: true });
+            dispatch({ type: 'SET_LOADING', payload: false });
          });
 
       return () => (isMounted = false);
    }, [id]);
 
+   if (isLoading) {
+      return <div>Loading...</div>;
+   }
+   if (error) {
+      return <div>{error}</div>;
+   }
+
    return (
       <div>
-         <h1>show page</h1>
-         {isLoading && <h2>Loading...</h2>}
-         {error}
-         {!error && !isLoading && (
-            <pre>{JSON.stringify(show, undefined, 3)}</pre>
-         )}
+         <ShowMainData
+            image={show.image}
+            name={show.name}
+            rating={show.rating}
+            summary={show.summary}
+            tags={show.genre}
+         />
+         <div>
+            <h2>Details</h2>
+            <Details
+               status={show.status}
+               network={show.network}
+               premiered={show.premiered}
+            />
+         </div>
+         <div>
+            <h2>Seasons</h2>
+            <Seasons seasons={show._embedded.seasons} />
+         </div>
+         <div>
+            <h2>Cast</h2>
+            <Cast seasons={show._embedded.cast} />
+         </div>
       </div>
    );
 };
